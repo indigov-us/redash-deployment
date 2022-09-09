30817f9e237f8ad3b6b6fa8673f0f7b748d19485
Ben Jacobson <ben.jacobson@indigov.us>
2022-09-09 12:04:36 -0600

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "hello from custom redash entrypoint"
+
 export PYTHONUNBUFFERED=1
+
+export REDASH_DATABASE_URL="${DATABASE_URL}"
+export REDIS_HOSTPORT="${REDIS_URL}"
 
 NCPUS="$(nproc)"
 export REDASH_WEB_WORKERS="${REDASH_WEB_WORKERS:-$NCPUS}"
